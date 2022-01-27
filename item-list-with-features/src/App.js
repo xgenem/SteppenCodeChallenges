4b7034a30df0637862ca30ad817d103e91b27343
Gene <eug5ne@gmail.com>
2022-01-27 18:58:52 +0800

    item list with features challenge

--- item-list-with-features/src/App.js
+++ item-list-with-features/src/App.js
@@ -1,34 +1,100 @@
-import { ItemList } from "./ItemList.jsx"
+import "bulma/css/bulma.min.css";
+import "./App.css";
+import { ItemList } from "./ItemList.jsx";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+import { v4 as uuid } from "uuid";
+import _ from "lodash";
 
-import "bulma/css/bulma.min.css"
+library.add(faTrash, faEdit);
 
 function App() {
+  const [data, setData] = useState([]);
+  const [text, setText] = useState();
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [editingId, setEditingId] = useState();
+
+  const handleOnChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleAddItem = () => {
+    if (isEditMode) {
+      let _data = [...data];
+      let index = _.findIndex(_data, { id: editingId });
+      _data[index].text = text;
+      setEditingId(null);
+      setIsEditMode(false);
+      setData(_data);
+    } else {
+      const newData = data.concat({
+        text,
+        id: uuid(),
+        shown: false,
+      });
+
+      setData(newData);
+    }
+    setText("");
+  };
+
+  const handleEdit = (id) => {
+    let index = _.findIndex(data, { id: id });
+    let item = data[index];
+
+    if (item) {
+      setIsEditMode(true);
+      setEditingId(item.id);
+      setText(item.text);
+    }
+  };
+
+  const handleRemove = (id) => {
+    const newData = data.filter((i) => i.id !== id);
+    setData(newData);
+  };
+
+  const handleOnKeyDown = (e) => {
+    if (e.key === "Enter") {
+      handleAddItem();
+    }
+  };
+
+  const handleShowButtons = (id) => {
+    let _data = [...data];
+    let index = _.findIndex(data, { id });
+    _data[index].shown = true;
+    setData(_data);
+  };
+
+  const handleHideButtons = (id) => {
+    let _data = [...data];
+    let index = _.findIndex(data, { id });
+    _data[index].shown = false;
+    setData(_data);
+  };
+
   return (
     <section className="section">
-      <div className="box">
-        <h1 className="title">Item List with Add, Edit and Remove</h1>
-        <p>Open <code>ItemList.jsx</code> and implement your component named <code>ItemList</code>.</p>
+      <ItemList
+        data={data}
+        handleEdit={handleEdit}
+        handleRemove={handleRemove}
+        showButtons={handleShowButtons}
+        hideButtons={handleHideButtons}
+      />
+      <div className="box columns control" style={{ marginTop: 20 }}>
+        <input
+          className="column input is-focused"
+          type="text"
+          value={text}
+          onChange={handleOnChange}
+          onKeyDown={handleOnKeyDown}
+        />
       </div>
-
-      <h3 className="title is-4">Task</h3>
-      <div className="content">
-        <p>Create a list of text items, with add, edit and remove functionality:</p>
-        <ul>
-          <li>Start the list empty.</li>
-          <li>The last item in the list is a text field where you can enter free text.</li>
-          <li>When the text field is submitted (either with a button or by pressing enter), the text in the field is added to the list as a new item.</li>
-          <li>Display each of the items as a separate paragraph when added.</li>
-          <li>When you hover over a list item, two buttons appear to the left of it - a delete button and an edit button.</li>
-          <li>Pressing the delete button asks for confirmation and then removes the item from the list.</li>
-          <li>Pressing the edit button turns the list item back into a text field, and when that field is submitted (either with a button or by pressing enter) it updates the list item.</li>
-        </ul>
-        <p>Use <code>bulma</code> classes for styling - the library is included in the bundle already.</p>
-      </div>
-
-      <h3 className="title is-4">Test</h3>
-      <ItemList />
     </section>
-  )
+  );
 }
 
-export default App
+export default App;
--- item-list-with-features/src/ItemList.jsx
+++ item-list-with-features/src/ItemList.jsx
@@ -1,3 +1,50 @@
-export const ItemList = ({ data }) => {
-  return null
-}
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+export const ItemList = ({
+  data,
+  handleRemove,
+  handleEdit,
+  showButtons,
+  hideButtons,
+}) => {
+  return (
+    <form>
+      {data?.map((item) => {
+        return (
+          <div
+            className="box columns"
+            key={item.id}
+            style={{ paddingTop: 0, paddingBottom: 0, marginTop: 10 }}
+            onMouseEnter={() => showButtons(item.id)}
+            onMouseLeave={() => hideButtons(item.id)}
+          >
+            <button
+              className="button"
+              onClick={(e) => {
+                e.preventDefault();
+                handleEdit(item.id);
+              }}
+              style={{ display: item.shown ? "block" : "none" }}
+            >
+              <span className="icon is-small has-text-info">
+                <FontAwesomeIcon icon="edit" />
+              </span>
+            </button>
+
+            <button
+              className="button"
+              onClick={() => handleRemove(item.id)}
+              style={{ display: item.shown ? "block" : "none" }}
+            >
+              <span className="icon is-small has-text-danger">
+                <FontAwesomeIcon icon="trash" />
+              </span>
+            </button>
+            <div className="column">
+              <p>{item.text}</p>
+            </div>
+          </div>
+        );
+      })}
+    </form>
+  );
+};
